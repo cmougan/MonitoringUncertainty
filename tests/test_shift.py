630f8f0e5cec40112e44318c8ddec9b5dfeec387
Carlos Mougan <carmougan@gmail.com>
2021-05-14 11:57:29 +0200

    #2

--- tests/test_shift.py
+++ tests/test_shift.py
@@ -11,9 +11,45 @@ import random
 
 class TestDistributionShift(unittest.TestCase):
     def setUp(self):
-        self.X = 2
-        self.y = 3
+        self.X = np.array([[1.0, 2.0, 3.0], [3.0, 4.0, 5.0]])
+        self.y = np.array([0, 1.0, 2.0])
 
     def test_strategy_assert(self):
         with pytest.raises(ValueError):
-            DistributionShift(strategy='aa')
+            DistributionShift(strategy="aa")
+
+    def test_param_float(self):
+        with pytest.raises(ValueError):
+            DistributionShift(param="aa")
+
+    def test_covariateShift(self):
+
+        # Param = 1
+        db = DistributionShift(param=1.0)
+        db.fit(self.X, self.y)
+        np.testing.assert_equal(db.transform(self.X).mean(axis=0), np.array([3, 4, 5]))
+
+        # Param = 2
+        db = DistributionShift(param=2.0)
+        db.fit(self.X, self.y)
+        np.testing.assert_equal(db.transform(self.X).mean(axis=0), np.array([4, 5, 6]))
+
+        # Param = 10
+        db = DistributionShift(param=10.0)
+        db.fit(self.X, self.y)
+        np.testing.assert_equal(
+            db.transform(self.X).mean(axis=0), np.array([12, 13, 14])
+        )
+
+        # On transform
+        db = DistributionShift(param=1.0)
+        db.fit(self.X, self.y)
+        np.testing.assert_equal(
+            db.transform(self.X, parameter=1).mean(axis=0), np.array([3, 4, 5])
+        )
+        np.testing.assert_equal(
+            db.transform(self.X, parameter=2).mean(axis=0), np.array([4, 5, 6])
+        )
+        np.testing.assert_equal(
+            db.transform(self.X, parameter=10).mean(axis=0), np.array([12, 13, 14])
+        )
